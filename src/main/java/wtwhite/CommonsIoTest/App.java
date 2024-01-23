1b8741d93ddfd6b08cb3f22aa344b2b974fd56e8
Tim White <walton.timothy.james.white@gmail.com>
2024-01-24 12:53:17 +1300

    Rename package to valid name

--- src/main/java/wtwhite/CommonsIoTest/App.java
+++ src/main/java/wtwhite/CommonsIoTest/App.java
@@ -1,4 +1,4 @@
-package wtwhite-test;
+package wtwhite.CommonsIoTest;
 
 /**
  * Hello world!
--- src/test/java/wtwhite/CommonsIoTest/AppTest.java
+++ src/test/java/wtwhite/CommonsIoTest/AppTest.java
@@ -1,4 +1,4 @@
-package wtwhite-test;
+package wtwhite.CommonsIoTest;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
